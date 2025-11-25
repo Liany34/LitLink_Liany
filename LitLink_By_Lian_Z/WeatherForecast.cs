fccd4d703643985cd5a155ef2fb14a82de69aab8
Liany34 <lianzazon4@gmail.com>
2025-11-25 12:56:18 +0200

    save

---
+++ LitLink_By_Lian_Z/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace LitLink_By_Lian_Z_
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
